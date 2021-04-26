b0ee0b996db990c0f3c3abea394ad183ce3db4e3
Hemanth <viruhemanth@gmail.com>
2021-04-26 15:27:08 +0530

    heroku bug

--- index.js
+++ index.js
@@ -131,10 +131,4 @@ app.post("/api/create", function (request, reply) {
 });
 
 // Run the server!
-app.listen(process.env.PORT || 3000, function (err, address) {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-  app.log.info(`server listening on ${address}`);
-});
+app.listen(process.env.PORT || 3000, "0.0.0.0");
